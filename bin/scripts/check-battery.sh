c734396980c430033cf6d2c8f684f40f832911f5
MT <minhtrietdiep@gmail.com>
2015-09-26 15:15:37 +0200

    Things

--- bin/scripts/check-battery.sh
+++ bin/scripts/check-battery.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
-# a tiny script to warn the user when battery is low
-# also puts the laptop to sleep when battery is crit
+
+################################################################
+#
+# Usage: check-battery
+#
+# A tiny script to warn the user when the battery is
+# low. It also puts the laptop to sleep when battery
+# is critically low.
+#
+################################################################
 
 status=$(cat /sys/class/power_supply/BAT1/status)
 percent=$(cat /sys/class/power_supply/BAT1/capacity)
-percent_low="12"
-percent_crit="7"
+percent_low="99"
+percent_crit="10"
 
-if [[ "$percent" -lt "$percent_crit" && "$status" == "Discharging" ]]; then
-        /usr/sbin/pm-suspend
-fi
+nag(){
+    for p in $(pgrep gconf-helper); do
+        dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$p/environ | sed 's/DBUS_SESSION_BUS_ADDRESS=//')
+        user=$(grep -m 1 -z USER /proc/$p/environ | sed 's/USER=//')
+        dply=$(grep -z DISPLAY /proc/$p/environ | sed 's/DISPLAY=//')
+        sudo -u $user sh -c "DBUS_SESSION_BUS_ADDRESS=\"$dbus\" DISPLAY=\"$dply\" i3-nagbar $@"
+    done
+}
 
 if [[ "$status" == "Charging" ]]; then
 	if [[ -f /tmp/battery_warning ]]; then
 		rm /tmp/battery_warning
-		exit
 	fi
+	exit
+fi
+
+if [[ "$percent" -lt "$percent_crit" && "$status" == "Discharging" ]]; then
+	systemctl suspend
 fi
 
 if [[ "$percent" -lt "$percent_low" ]]; then
@@ -23,7 +40,8 @@ if [[ "$percent" -lt "$percent_low" ]]; then
 		exit
 	fi
 	touch /tmp/battery_warning
-	DISPLAY=:0.0 /usr/bin/i3-nagbar -m 'Battery is almost empty!'
+	nag -m 'Battery is almost empty!'
+	# DISPLAY=:0.0 /usr/bin/notify-send "Battery is almost empty!"
 else
 	if [[ -f /tmp/battery_warning ]]; then
 		rm /tmp/battery_warning
